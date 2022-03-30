11d53731c1b66f17a33e97d60a22cb5bd36625ab
YaChugunov <informed.looser@gmail.com>
2022-03-30 22:36:03 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+
+function Stars(props) {
+  return (
+    <svg
+      fill="#D3BCA2"
+      height="28"
+      viewBox="0 0 18 18"
+      width="28"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" />
+      <path d="M0 0h18v18H0z" fill="none" />
+    </svg>
+  );
+}
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <ul class="card-body-stars u-clearfix">
+        <li>
+          <Stars count={1} />
+        </li>
+      </ul>
     </div>
   );
 }
