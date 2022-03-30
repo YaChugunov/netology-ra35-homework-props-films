a145d2d3d0b9ec3a76da2efe5673df9e9c8c61e3
YaChugunov <informed.looser@gmail.com>
2022-03-31 00:37:30 +0300

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './style.css';
 
-function Stars(props) {
-  return (
-    <svg
-      fill="#D3BCA2"
-      height="28"
-      viewBox="0 0 18 18"
-      width="28"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" />
-      <path d="M0 0h18v18H0z" fill="none" />
-    </svg>
-  );
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Обновить состояние с тем, чтобы следующий рендер показал запасной UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Можно также сохранить информацию об ошибке в соответствующую службу журнала ошибок
+    logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Можно отрендерить запасной UI произвольного вида
+      return <h1>Что-то пошло не так.</h1>;
+    }
+
+    return this.props.children;
+  }
 }
+
+function Stars(props) {
+  const htmlStar = props.icon;
+  const countStar = props.count;
+
+  let htmlString = '';
+
+  if (props.count < 1 || props.count > 5 || typeof props.count != 'number') {
+    return null;
+  }
+  for (let i = 1; i <= countStar; i++) {
+    htmlString = htmlString + htmlStar;
+  }
+  // console.log(out);
+
+  return <div dangerouslySetInnerHTML={{ __html: htmlString }}></div>;
+}
+
+Stars.defaultProps = {
+  count: 0,
+  icon: '<svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28"  xmlns="http://www.w3.org/2000/svg"><path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" /><path d="M0 0h18v18H0z" fill="none" /></svg>',
+};
+
+Stars.propTypes = {
+  count: PropTypes.number,
+};
 
 export default function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+// import ReactDOM from "react-dom";
+import * as ReactDOMClient from 'react-dom/client';
 
-import App from "./App";
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+const container = document.getElementById('root');
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+// Initial render: Render an element to the root.
+root.render(<App />);
